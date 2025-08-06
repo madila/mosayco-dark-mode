59730b9404bae1d88c442da36b2d7b00e3bafdb3
madila <contact@rubenmadila.com>
2025-08-06 12:26:28 +0100

    Updating dependencies

--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '15b4e01f32434bea5273', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '91abfa0def2f52d21dc0', 'type' => 'module');
